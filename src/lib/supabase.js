bc29d09f3b46b00a8f83f6cdbed1bb4a33fdf019
salumusabri05 <salumusabri05@gmail.com>
2025-07-10 16:41:30 +0300

    reversed

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://mavaujxjkzyuhphpgtue.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdmF1anhqa3p5dWhwaHBndHVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2Mjc3NTIsImV4cCI6MjA2NTIwMzc1Mn0.gT90RdhPzgFQ7jfY6T0yB-Cb2ccpSTxUPfHoAyob2L4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

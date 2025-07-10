a63cc24751dd4e488110e27fd510244063addb19
salumusabri05 <salumusabri05@gmail.com>
2025-07-10 16:32:35 +0300

    env outside

--- src/lib/supabase-admin.js
+++ src/lib/supabase-admin.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a separate admin client with service_role key
-const supabaseUrl = 'https://mavaujxjkzyuhphpgtue.supabase.co';
-const supabaseServiceKey= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdmF1anhqa3p5dWhwaHBndHVlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTYyNzc1MiwiZXhwIjoyMDY1MjAzNzUyfQ.COa3zYBPHAHEYJvpUwqxQ9k97FUMyahuhT5T-cPM9vw'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
 
